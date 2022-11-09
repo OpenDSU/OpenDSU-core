5e7e95802416b14856a58ebb86b020a5c6eaef6c
skutner <sava.dumitru.daniel@gmail.com>
2022-11-09 19:09:27 +0200

    Pharmaledger-IMI/epi-workspace#1019 append data to brick map to a previously embedded file

--- tests/resolver/basic/writeEmbeddedFileTest.js
+++ tests/resolver/basic/writeEmbeddedFileTest.js
@@ -9,15 +9,23 @@ const resolver = openDSU.loadAPI("resolver");
 const DOMAIN = "default";
 const FILEPATH = "/folder/file1";
 const FILE_CONTENT = "some content";
+const NEW_FILE_CONTENT = "some other content";
+const APPENDED_FILE_CONTENT = "appended content";
 assert.callback('write embedded file test', (testFinished) => {
     dc.createTestFolder('writeEmbeddedFile', async (err, folder) => {
         await tir.launchConfigurableApiHubTestNodeAsync({rootFolder: folder});
         const seedDSU = await $$.promisify(resolver.createSeedDSU)(DOMAIN);
         await $$.promisify(seedDSU.embedFile)(FILEPATH, FILE_CONTENT);
         let dsuContent = await $$.promisify(seedDSU.readFile)(FILEPATH);
-        dsuContent = dsuContent.toString();
-        assert.equal(dsuContent, FILE_CONTENT);
+        assert.equal(dsuContent.toString(), FILE_CONTENT);
 
+        await $$.promisify(seedDSU.writeFile)(FILEPATH, NEW_FILE_CONTENT);
+        dsuContent = await $$.promisify(seedDSU.readFile)(FILEPATH);
+        assert.equal(dsuContent.toString(), NEW_FILE_CONTENT);
+
+        await $$.promisify(seedDSU.appendToFile)(FILEPATH, APPENDED_FILE_CONTENT);
+        dsuContent = await $$.promisify(seedDSU.readFile)(FILEPATH);
+        assert.equal(dsuContent.toString(), NEW_FILE_CONTENT + APPENDED_FILE_CONTENT);
         testFinished();
     });
 }, 5000);
