b7c70ee24636fa59bfdcb604cc9e867dca5fbf37
skutner <sava.dumitru.daniel@gmail.com>
2022-11-28 10:46:19 +0200

    updated PathKeySSITest.js

--- tests/keyssi/PathKeySSITest.js
+++ tests/keyssi/PathKeySSITest.js
@@ -4,15 +4,9 @@ const assert = dc.assert;
 const openDSU = require('../../index');
 $$.__registerModule("opendsu", openDSU);
 const enclaveAPI = openDSU.loadAPI("enclave");
-const resolver = openDSU.loadAPI("resolver");
 const keySSISpace = openDSU.loadAPI("keyssi");
 const scAPI = openDSU.loadAPI("sc");
-const configAPI = openDSU.loadAPI("config");
-const w3cDID = openDSU.loadAPI("w3cdid");
-
-const EnclaveHandler = require("../../enclave/impl/WalletDBEnclaveHandler");
 const tir = require("../../../../psknode/tests/util/tir");
-const constants = require("../../moduleConstants");
 assert.callback('WalletDBEnclave test', (testFinished) => {
     dc.createTestFolder('createDSU', async (err, folder) => {
         const vaultDomainConfig = {
@@ -25,15 +19,18 @@ assert.callback('WalletDBEnclave test', (testFinished) => {
 
         const mainEnclave = enclaveAPI.initialiseWalletDBEnclave();
         mainEnclave.on("initialised", async () => {
-
             await $$.promisify(scAPI.setMainEnclave)(mainEnclave);
             const sc = scAPI.refreshSecurityContext();
             sc.on("initialised", async () => {
-                const pathKeySSI = await $$.promisify(keySSISpace.createPathKeySSI)("vault", "0/somePath")
-                const expectedIdentifier = "ssi:path:vault:0/somePath::v0"
-                assert.equal(pathKeySSI.getIdentifier(true), expectedIdentifier);
+                const gtin = "00000000000000"
+                const batchId = "b1";
+                const pathKeySSI = await $$.promisify(keySSISpace.createPathKeySSI)("vault", `0/${gtin}/${batchId}`);
+                const seedSSI = await $$.promisify(pathKeySSI.derive)();
+                const newPathKeySSI = await $$.promisify(keySSISpace.createPathKeySSI)("vault", `0/${gtin}/${batchId}`);
+                const newSeedSSI = await $$.promisify(newPathKeySSI.derive)();
+                assert.equal(seedSSI.getIdentifier(), newSeedSSI.getIdentifier(), "SeedSSIs should be the same")
                 testFinished();
             });
         });
     });
-}, 100000);
+}, 10000);
