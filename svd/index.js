8c1a1195ea293714751e49bab663d6a24e366aa6
Roxana-Irimia <roxana@axiologic.net>
2023-06-20 11:11:59 +0300

    Update svd API

--- svd/index.js
+++ svd/index.js
@@ -1,3 +1,21 @@
-const FastSVD  = require("fast-svd").FastSVD
+const FastSVD = require("fast-svd").FastSVD
 
-module.exports = FastSVD
+class FsSVDStorage {
+    constructor(directory) {
+        this.persistence = FastSVD.createFSPersistence(directory);
+        this.factory = new FastSVD.createFactory(this.persistence);
+    }
+    registerType(typeName, typeDescription) {
+        this.factory.registerType(typeName, typeDescription);
+    }
+    createSession() {
+        return new FastSVD.createSession(this.factory);
+    }
+}
+
+const createFsSVDStorage = (directory) => {
+    return new FsSVDStorage(directory);
+}
+module.exports = {
+    createFsSVDStorage
+}
