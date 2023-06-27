6aca0864a8b14476de58f22903d76343c44e209a
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-06-27 15:07:22 +0300

    adding a console.trace to debug strange behaviour

--- crypto/index.js
+++ crypto/index.js
@@ -38,6 +38,7 @@ const hashSync = (keySSI, data) => {
 
 const encrypt = (data, encryptionKey) => {
     const logger = $$.getLogger("encrypt", "opendsu/crypto");
+console.trace("Check who is calling the encrypt method");
     logger.info(0x900, "DSUs are encrypted using AES-GCM 256bit");
     const pskEncryption = crypto.createPskEncryption("aes-256-gcm");
     return pskEncryption.encrypt(data, encryptionKey);
