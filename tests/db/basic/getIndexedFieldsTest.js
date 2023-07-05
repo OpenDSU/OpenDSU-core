55daf2cfdcb4d7b47cdfe1c351f4c41db9b293f9
skutner <sava.dumitru.daniel@gmail.com>
2023-07-05 14:05:36 +0300

    fixed test

--- tests/db/basic/getIndexedFieldsTest.js
+++ tests/db/basic/getIndexedFieldsTest.js
@@ -10,11 +10,9 @@ const double_check = require("double-check");
 const assert = double_check.assert;
 const db = require("../../../db");
 const tir = require("../../../../../psknode/tests/util/tir");
+$$.LEGACY_BEHAVIOUR_ENABLED = true;
 
-require("callflow").initialise();
-//ow.register("opendsu", "../index.js")
-
-$$.flows.describe("FilterDB", {
+const indexedFields = {
     start: function (callback) {
         this.callback = callback;
 
@@ -81,8 +79,7 @@ $$.flows.describe("FilterDB", {
             this.callback();
         });
     }
-});
-
+}
 assert.callback("DB filtering test", (callback) => {
-    $$.flows.start("FilterDB", "start", callback);
+    indexedFields.start(callback);
 }, 10000);
