c6cf9a5ba24f238daaea53ca369db354c081548a
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2023-09-07 19:16:12 +0300

    bug fixing

--- m2dsu/defaultApis/index.js
+++ m2dsu/defaultApis/index.js
@@ -91,7 +91,6 @@ registry.defineApi("registerDSU", async function (dsu) {
         this.groupInstance.dsuInstancesCache = {};
     }
 
-    //TODO: temporary fix, this apiRegistry is now instantiated for each mapping message
     if (!dsu.batchInProgress()) {
         this.registeredDSUs.push(dsu);
         if(this.groupInstance){
