c645121ac3147d68d2b8db4fe04bb99278050dda
skutner <sava.dumitru.daniel@gmail.com>
2023-11-04 13:35:27 +0200

    axiologic-pla/dev#611 axiologic-pla/dev#616 bug fixes

--- tests/enclave/WaletDBEnclaveInitFailsTest.js
+++ tests/enclave/WaletDBEnclaveInitFailsTest.js
@@ -1,12 +1,14 @@
 require("../../../../builds/output/testsRuntime");
 const tir = require("../../../../psknode/tests/util/tir");
-
+$$.debug.verbosity("debug")
 const dc = require("double-check");
 const assert = dc.assert;
 const openDSU = require('../../index');
 $$.__registerModule("opendsu", openDSU);
 const enclaveAPI = openDSU.loadAPI("enclave");
 const keySSISpace = openDSU.loadAPI("keyssi");
+$$.debug.useStdoutOnceForErrorWithCode(0x401);
+
 assert.callback('WalletDBEnclave init fail test', async (testFinished) => {
     const DOMAIN = "default";
     const rootFolder = await $$.promisify(dc.createTestFolder)("createDSU");
@@ -14,7 +16,6 @@ assert.callback('WalletDBEnclave init fail test', async (testFinished) => {
     const seedSSI = await $$.promisify(keySSISpace.createSeedSSI)(DOMAIN);
     console.log(seedSSI.getIdentifier());
     const walletDBEnclave = enclaveAPI.initialiseWalletDBEnclave(seedSSI);
-    $$.debug.useStdoutOnceForErrorWithCode(0x401);
     walletDBEnclave.on("error", (err) => {
         assert.true(err !== undefined);
         testFinished();
