368a47e3707c084aba20be366f1fff05b2f27be0
skutner <sava.dumitru.daniel@gmail.com>
2025-03-26 10:52:50 +0200

    Refactor ServerlessClient to simplify instance handling
    Replaced class-based instance management with a proxy-focused model for streamlined command execution and event handling. Updated the `createServerlessAPIClient` function to directly return the refactored `ServerlessClient` instance.

--- serverless/ServerlessClient.js
+++ serverless/ServerlessClient.js
@@ -6,65 +6,72 @@ function ServerlessClient(userId, endpoint, serverlessId, pluginName, webhookUrl
         throw new Error('Endpoint URL is required');
     }
 
-    this.userId = userId;
-    this.endpoint = endpoint;
-    this.serverlessId = serverlessId;
-    this.pluginName = pluginName;
-    this.webhookUrl = webhookUrl;
-    this.eventEmitter = new EventEmitter();
-    this.baseEndpoint = `${endpoint}/proxy`;
-    this.commandEndpoint = `${this.baseEndpoint}/executeCommand/${serverlessId}`;
+    const eventEmitter = new EventEmitter();
+    const baseEndpoint = `${endpoint}/proxy`;
+    const commandEndpoint = `${baseEndpoint}/executeCommand/${serverlessId}`;
 
-    this.__executeCommand = function(commandName, args) {
-        args = args || [];
-        const command = {
-            forWhom: this.userId,
-            name: commandName,
-            pluginName: this.pluginName,
-            args: args
-        };
+    const instance = {
+        __executeCommand: function(commandName, args) {
+            args = args || [];
+            const command = {
+                forWhom: userId,
+                name: commandName,
+                pluginName,
+                args: args
+            };
 
-        // Create response instance before fetch
-        // Initially create as sync type, will be updated after we know the actual type
-        const clientResponse = new LambdaClientResponse(this.webhookUrl, null, 'sync');
+            // Create response instance before fetch
+            // Initially create as sync type, will be updated after we know the actual type
+            const clientResponse = new LambdaClientResponse(webhookUrl, null, 'sync');
 
-        fetch(this.commandEndpoint, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(command)
-        }).then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        }).then(res => {
-            if (!this.webhookUrl && (res.operationType === 'slowLambda' || res.operationType === 'observableLambda')) {
-                throw new Error('Webhook URL is required for async operations');
-            }
+            fetch(commandEndpoint, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(command)
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            }).then(res => {
+                if (!webhookUrl && (res.operationType === 'slowLambda' || res.operationType === 'observableLambda')) {
+                    throw new Error('Webhook URL is required for async operations');
+                }
 
-            if (res.operationType === 'sync') {
-                clientResponse._resolve(res.result);
-            } else {
-                clientResponse._updateOperationType(res.operationType);
-                clientResponse._setCallId(res.result);
-            }
-        }).catch(error => {
-            this.eventEmitter.emit('error', {
-                commandName,
-                error: error.message || String(error)
+                if (res.operationType === 'sync') {
+                    clientResponse._resolve(res.result);
+                } else {
+                    clientResponse._updateOperationType(res.operationType);
+                    clientResponse._setCallId(res.result);
+                }
+            }).catch(error => {
+                eventEmitter.emit('error', {
+                    commandName,
+                    error: error.message || String(error)
+                });
+                clientResponse._reject(error);
             });
-            clientResponse._reject(error);
-        });
 
-        return clientResponse;
+            return clientResponse;
+        },
+
+        onError: function(callback) {
+            eventEmitter.on('error', callback);
+            return () => eventEmitter.off('error', callback);
+        }
     };
 
-    this.onError = function(callback) {
-        this.eventEmitter.on('error', callback);
-        return () => this.eventEmitter.off('error', callback);
-    };
+    return new Proxy(instance, {
+        get(target, prop, receiver) {
+            if (prop in target) {
+                return target[prop];
+            }
+
+            return (...args) => target.__executeCommand(prop, args);
+        }
+    });
 }
 
 module.exports = ServerlessClient; 
--- serverless/index.js
+++ serverless/index.js
@@ -1,17 +1,7 @@
 const ServerlessClient = require('./ServerlessClient');
 
 function createServerlessAPIClient(userId, endpoint, serverlessId, pluginName, webhookUrl) {
-    const client = new ServerlessClient(userId, endpoint, serverlessId, pluginName, webhookUrl);
-    
-    return new Proxy(client, {
-        get(target, prop, receiver) {
-            if (prop in target) {
-                return target[prop];
-            }
-
-            return (...args) => target.__executeCommand(prop, args);
-        }
-    });
+    return new ServerlessClient(userId, endpoint, serverlessId, pluginName, webhookUrl);
 }
 
 module.exports = {
