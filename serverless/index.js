e7e7ffa693edcfd28a8540d72668c8abfd27730a
alupoaiemircea <mircea@axiologic.net>
2025-03-31 11:58:11 +0300

    fix init client

--- serverless/index.js
+++ serverless/index.js
@@ -1,7 +1,7 @@
 const ServerlessClient = require('./ServerlessClient');
 
-async function createServerlessAPIClient(userId, endpoint, serverlessId, pluginName, webhookUrl) {
-    const client = new ServerlessClient(userId, endpoint, serverlessId, pluginName, webhookUrl);
+async function createServerlessAPIClient(userId, endpoint, serverlessId, pluginName, webhookUrl, options) {
+    const client = new ServerlessClient(userId, endpoint, serverlessId, pluginName, options);
     return await client.init();
 }
 
